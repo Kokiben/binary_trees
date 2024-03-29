1b7aab966499dfd8e6ce3bb60649ebc7f1809f69
Kokiben <bennassarkaoutar@gmail.com>
2024-03-29 16:45:30 -0700

    balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,19 +9,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+size_t Left_Heigh = 0, Right_Heigh = 0;
 
-size_t Left_Heigh, Right_High;
 if (tree == NULL)
-{
-return (0); /* If tree is NULL, returns 0. */
-}
-/* Recursively compute the height of the left subtree */
-Left_Heigh = binary_tree_height(tree->left);
-/* Recursively compute the height of the right subtree */
-Right_High = binary_tree_height(tree->right);
-/* Return the max of the left and right subtree heights */
-return ((Left_Heigh > Right_High ? Left_Heigh : Right_High)+ 1);
+return (0);/* If tree is NULL, returns 0. */
 
+if (tree->left)
+/* Recursively compute the height of the left subtree */
+Left_Heigh = 1 + binary_tree_height(tree->left);
+if (tree->right)
+/* Recursively compute the height of the right subtree */
+Right_Heigh = 1 + binary_tree_height(tree->right);
+/* Return the max of the left and right subtree heights */
+return ((Left_Heigh > Right_Heigh) ? Left_Heigh : Right_Heigh);
 }
 
 /**
