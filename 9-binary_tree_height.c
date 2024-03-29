96ff53eec1dcf34573f5f5f9408d5b4ffba67739
Kokiben <bennassarkaoutar@gmail.com>
2024-03-29 16:30:34 -0700

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,26 +9,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-
-size_t height = 0;
-const binary_tree_t *current = tree;
-stack_t *stack = NULL;
+size_t l = 0, r = 0;
 
 if (tree == NULL)
 return (0);
 
-while (current != NULL || !stack_empty(stack))
-{
-while (current != NULL)
-{
-stack_push(&stack, current);
-current = current->left;
-height++;
-}
-
-current = stack_pop(&stack);
-current = current->right;
-}
-
-return (height - 1); /* Subtract 1 to get the height of the tree */
+if (tree->left)
+l = 1 + binary_tree_height(tree->left);
+if (tree->right)
+r = 1 + binary_tree_height(tree->right);
+return ((l > r) ? l : r);
 }
